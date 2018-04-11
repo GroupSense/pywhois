25a09fdc391e1fef70e6ed481fc675c7d1543635
Garry Harthill <gazzerh@gmail.com>
2018-04-11 17:17:31 +0100

    fix eu regex

--- whois/parser.py
+++ whois/parser.py
@@ -723,7 +723,7 @@ class WhoisEu(WhoisEntry):
         'tech_fax': r'Technical: *Name: *[^\n\r]+\s*Organisation: *[^\n\r]+\s*Language: *[^\n\r]+\s*Phone: *[^\n\r]+\s*Fax: *([^\n\r]+)',
         'tech_email': r'Technical: *Name: *[^\n\r]+\s*Organisation: *[^\n\r]+\s*Language: *[^\n\r]+\s*Phone: *[^\n\r]+\s*Fax: *[^\n\r]+\s*Email: *([^\n\r]+)',
         'registrar': r'Registrar: *Name: *([^\n\r]+)',
-        'name_servers': r'Name servers: *([^\n\r]+)\s*([^\n\r]*)',  # list of name servers
+        'name_servers': r'Name servers:\s*(.+)',  # list of name servers
     }
 
     def __init__(self, domain, text):
